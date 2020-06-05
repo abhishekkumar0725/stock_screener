330691b6be3d755658f94d6514b52226c5e65922
abhishek <Abhishek Kumar>
2020-06-04 21:07:20 -0400

    Basic Alpha VantageTests Completed

--- dataRetrieval.py
+++ dataRetrieval.py
@@ -1,2 +1,15 @@
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.sectorperformance import SectorPerformances
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+import pandas as pd
 
-KEY='WWOYKJ31LZOV8QZF'
+key='WWOYKJ31LZOV8QZF'
+
+df = pd.read_csv('data/securities.csv')
+
+ts = TimeSeries(key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+print(data)
