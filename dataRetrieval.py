5646a8ada366257309005a7cf3689574cfae6da4
abhishek <Abhishek Kumar>
2020-06-09 13:20:01 -0400

    Found best sectors

--- dataRetrieval.py
+++ dataRetrieval.py
@@ -5,11 +5,18 @@ from alpha_vantage.cryptocurrencies import CryptoCurrencies
 from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 import pandas as pd
+#from talib import *
 
 key='WWOYKJ31LZOV8QZF'
+securities = pd.read_csv('data/securities.csv')
 
-df = pd.read_csv('data/securities.csv')
+def getSectorData():
+    sp = SectorPerformances(key, output_format='pandas')
+    data, _ =  sp.get_sector()
+    data = data.sort_values(by=[data.columns[0]])
+    return data
 
-ts = TimeSeries(key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-print(data)
+def findBestSector(numSectors=3, metric=0):
+    data = getSectorData()
+    data = data.sort_values(by=[data.columns[metric]])
+    return data.iloc[:numSectors]
